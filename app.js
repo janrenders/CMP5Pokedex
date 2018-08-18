16ea0992feaf72d9a40e2a95d8b804d341d8564f
Jan Renders <>
2018-08-18 20:41:29 +0200

    Backend update added description, weight, height, types

--- app.js
+++ app.js
@@ -46,12 +46,38 @@ $(function(){
       method: "GET",
       dataType:"json",
     });
-    
+
 
     request.done(function( data ) {
       $('.nameText').text(data.name)
 
-      console.log(data)
+      $('.heightText').html(data.height /10 + "m")
+      $('.weightText').html(data.weight /10 + "kg")
+
+
+      var api2="https://pokeapi.co/api/v2/pokemon-species/" + data.id
+
+      $.getJSON(api2, function(data2){
+          $('.descriptionText').html(data2.flavor_text_entries[1].flavor_text);
+
+        })
+
+
+
+
+          $('.typesText').html(data.types[1].type.name + ", " + data.types[0].type.name);
+
+
+
+
+
+
+            $('.pokedex h3').text(data.name.toUpperCase())
+
+            $('.poke-img img').attr('src', data.sprites.front_default)
+
+        console.log(data)
+
     });
 
 
